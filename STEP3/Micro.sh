44c8883862757c0d0e150079aede6e3015b67f16
Axel Ward <35614781+AxelWard@users.noreply.github.com>
2020-05-03 12:47:22 -0600

    Last Script Update

--- STEP3/Micro.sh
+++ STEP3/Micro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java org.antlr.v4.Tool LITTLE.g4
+java -cp '.:antlr-4.8-complete.jar' org.antlr.v4.Tool LITTLE.g4
 javac -cp '.:antlr-4.8-complete.jar' *.java
 java -cp '.:antlr-4.8-complete.jar' Main $1 $2
